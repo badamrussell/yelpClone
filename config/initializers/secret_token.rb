4ea2bb9951ce7ca3629582c4345f05b2a98ed779
B. Adam Russell <b.ad.russell@gmail.com>
2013-12-11 14:35:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YelpClone::Application.config.secret_token = 'ba3352bb710ec38cfb809bb33c174e400f60b5bb1fa0c4988e92f5ecd2ec4176330b3ab3755e2573b1655fad2a0aa423a5afb68a6dcc56ea5cdf6707f66e4bad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+YelpClone::Application.config.session_store :cookie_store, key: '_YelpClone_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# YelpClone::Application.config.session_store :active_record_store
