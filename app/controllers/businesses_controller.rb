b695ad512718f453ca95682d83c20b796dd10e00
B. Adam Russell <b.ad.russell@gmail.com>
2013-12-11 21:10:16 -0500

    Actions defined in Business Controller

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,2 +1,24 @@
 class BusinessesController < ApplicationController
+  def show
+    @business = Business.find(params[:id])
+  end
+
+  def new
+    @business = Business.new()
+  end
+
+  def create
+    @business = Business.new(params[:business])
+
+    if @business.save
+      redirect_to business_url(@business.id)
+    else
+      flash[:errors] = @businesses.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+
+  end
 end
--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,7 @@
 class Business < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :country_id ,:name ,:address1 ,:address2 ,:city ,:state ,:zip_code ,:phone_number ,:website ,:rating ,:category1 ,:category2 ,:category3
+
+  validates :name, :country_id
+
 end
