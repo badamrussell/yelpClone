4865af464bc9559ea70e6e5793785ea7a68aaad9
B. Adam Russell <b.ad.russell@gmail.com>
2013-12-15 13:07:37 -0500

    finer filters layout on search page

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,20 @@
 class SearchesController < ApplicationController
   def show
     @breadcrumbs = { "Business" => search_url }
+    @finer_filters = nil
 
     if params["category_id"]
       crumb_category = Category.find(params["category_id"])
-      @breadcrumbs[MainCategory.find(crumb_category.id).name] = search_url(main_category_id: crumb_category.main_category_id)
+      main_name = MainCategory.find(crumb_category.id).name
+      @breadcrumbs[main_name] = search_url(main_category_id: crumb_category.main_category_id)
       @breadcrumbs[crumb_category.name] = ""
+    elsif params["main_category_id"]
+      main_name = MainCategory.find(params["main_category_id"]).name
+      @breadcrumbs[main_name] = ""
+      @finer_filters = Category.where(main_category_id: params["main_category_id"])
+    else
+      @breadcrumbs["Business"] = ""
+      @finer_filters = MainCategory.all
     end
 
     search_terms = {}
