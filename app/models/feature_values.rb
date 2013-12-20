0bf387a8445f4c9854da18b590ba4ceafbd94c6c
B. Adam Russell <b.ad.russell@gmail.com>
2013-12-20 14:31:54 -0500

    removed scope

--- app/models/feature_values.rb
+++ app/models/feature_values.rb
@@ -2,7 +2,6 @@ class FeatureValues < ActiveRecord::Base
   attr_accessible :feature_id, :name
 
   validates :feature_id, :name, presence: true
-  validates :feature_id, scope: [:name], uniqueness: true
 
   has_many(
     :features,
