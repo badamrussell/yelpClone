d2838f08f54f7b48c97e1034c1d9f2996c58389d
B. Adam Russell <b.ad.russell@gmail.com>
2014-01-08 17:07:18 -0500

    move vote counts to helpers

--- app/helpers/review_compliments_helper.rb
+++ app/helpers/review_compliments_helper.rb
@@ -1,2 +1,7 @@
 module ReviewComplimentsHelper
+
+  def all_compliments
+    @all_compliments ||= Compliment.all
+  end
+
 end
--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,15 @@
 module VotesHelper
+
+  def all_votes
+    @all_votes ||= Vote.all
+  end
+
+  def vote_counts
+    @vote_counts ||= Vote.joins(:review_votes).group(:review_id, "votes.id").count(:vote_id)
+  end
+
+  def user_votes
+    @user_votes ||= current_user ? current_user.review_votes : []
+  end
+
 end
