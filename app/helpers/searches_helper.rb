ccf165f1c92dc520cb11f8ab497bc39e51356a13
B. Adam Russell <b.ad.russell@gmail.com>
2014-01-14 15:22:57 -0500

    query construction more railsy

--- app/helpers/searches_helper.rb
+++ app/helpers/searches_helper.rb
@@ -143,131 +143,109 @@ module SearchesHelper
     size.times.map { "?" }.join(",")
   end
 
-  def make_where
+  def make_neighborhood(params, query)
+    return query unless params
 
-  end
-
-  def make_joins
-
-  end
-
-  def make_neighborhood(params, where_string, values)
-    if params
-      set = params.map { |n| n.to_i }
-      set.each { |s| values << s }
-
-      where_string << " AND " unless where_string.blank?
-      where_string << "neighborhood_id IN (#{q_set(set.length)})"
-    end
-  end
-
-  def make_feature(params, joins, wheres, values)
-    return nil unless params
-
-    joins << "JOIN business_features ON businesses.id = business_features.business_id"
-    set = params.map { |f| f.to_i }
-    wheres << "business_features.feature_id IN (#{q_set(set.length)})"
+    set = params.map { |n| n.to_i }
     set.each { |s| values << s }
+
+    query.where("neighborhood_id IN (#{q_set(set.length)})", *set)
   end
 
-  def make_category(params, joins, wheres, values)
-    return nil unless params
+  def make_feature(params, query)
+    return query unless params
 
-    joins << "JOIN business_categories ON businesses.id = business_categories.business_id"
+    set = params.map { |f| f.to_i }
+
+    query.joins("JOIN business_features ON businesses.id = business_features.business_id")
+        .where("business_features.feature_id IN (#{q_set(set.length)})", *set)
+  end
+
+  def make_category(params, query)
+    return query unless params
+
+    query = query.joins("JOIN business_categories ON businesses.id = business_categories.business_id")
+
     if params.length == 1
-      wheres << "business_categories.category_id = ?"
-      values << params[0].to_i
+      query.where("business_categories.category_id = ?", params[0].to_i)
     else
       set = params.map { |c| c.to_i }
-      wheres << "business_categories.category_id IN (#{q_set(set.length)})"
-      set.each { |s| values << s }
+      query.where("business_categories.category_id IN (#{q_set(set.length)})", *set)
     end
   end
 
-  def make_main_category(params, joins, wheres, values)
-    return nil unless params
+  def make_main_category(params, query)
+    return query unless params
 
     unless params
-      joins << "JOIN business_categories ON businesses.id = business_categories.business_id"
+      query = query.joins("JOIN business_categories ON businesses.id = business_categories.business_id")
     end
-    joins << "JOIN categories ON categories.id = business_categories.category_id"
 
-    wheres << "categories.main_category_id = ?"
-    values << params
+    query.joins("JOIN categories ON categories.id = business_categories.category_id")
+        .where("categories.main_category_id = ?", params)
   end
 
-  def make_price(params, wheres, values)
-    return nil unless params
+  def make_price(params, query)
+    return query unless params
 
     set = params.map { |p| p.to_i }
-
-    wheres << " businesses.price_range_avg IN (#{q_set(set.length)})"
-
-    set.each { |s| values << s }
+    query.where("businesses.price_range_avg IN (#{q_set(set.length)})", *set)
   end
 
-  def make_distance(params, wheres, values)
-    return nil unless params
+  def make_distance(params, query)
+    return query unless params
 
-    wheres << "businesses.latitude BETWEEN ? AND ?"
-    wheres << "businesses.longitude BETWEEN ? AND ?"
-
-    values << params[0]
-    values << params[2]
-    values << params[3]
-    values << params[1]
+    query.where("businesses.latitude BETWEEN ? AND ?", params[0], params[2])
+        .where("businesses.longitude BETWEEN ? AND ?", params[3], params[1])
   end
 
+  def build_query(query, search_params)
+
+    query = make_neighborhood(search_params[:neighborhood_id], query)
+    query = make_feature(search_params[:feature_id], query)
+    query = make_category(search_params[:category_id], query)
+    query = make_main_category(search_params[:main_category_id], query)
+    query = make_price(search_params[:price_range], query)
+    query = make_distance(search_params[:distance], query)
+
+  end
+
+  def build_order(query, search_string, search_params)
+    rank_string = ""
+
+    unless search_string.blank?
+      query = query.where("to_tsvector('simple', coalesce(businesses.name::text, '')) @@ to_tsquery('simple', " + Business.sanitize(search_string) + ")")
+                  .order("search_rank DESC")
+    end
+
+    if search_params[:sort]
+      if search_params[:sort] == "rated"
+        query = query.order("businesses.rating_avg DESC")
+      elsif search_params[:sort] == "reviewed"
+        query = query.order("businesses.reviews_count DESC")
+      end
+    end
+
+    query
+  end
 
   def rails_query(search_string, params, search_location)
     search_params = format_params_for_query(params)
 
-    wheres = []
-    joins = []
-    values = []
-    orders = []
-
     rank_string = ""
-    where_string = ""
-    order_string = ""
-
     if search_string.blank?
-      rank_string ="('0')"
+      rank_string = "('0')"
     else
       rank_string = "ts_rank(to_tsvector('simple', coalesce(businesses.name::text, '')), to_tsquery('simple', " + Business.sanitize(search_string) + "), 0)"
-      where_string << "to_tsvector('simple', coalesce(businesses.name::text, '')) @@ to_tsquery('simple', " + Business.sanitize(search_string) + ")"
-      orders << "search_rank DESC"
     end
 
-    make_neighborhood(search_params[:neighborhood_id], where_string, values)
-    make_feature(search_params[:feature_id], joins, wheres, values)
-    make_category(search_params[:category_id], joins, wheres, values)
-    make_main_category(search_params[:main_category_id], joins, wheres, values)
-    make_price(search_params[:price_range], wheres, values)
-    make_distance(search_params[:distance], wheres, values)
+    current_query = Business.select("businesses.*, #{rank_string} AS search_rank")
+                    .includes(:categories, :photos, :neighborhood, :top_review)
 
-    if search_params[:sort]
-      if search_params[:sort] == "rated"
-        orders.unshift("businesses.rating_avg DESC")
-      elsif search_params[:sort] == "reviewed"
-        orders.unshift("businesses.reviews_count DESC")
-      end
-    end
+    current_query = build_query(current_query,search_params)
+    current_query = build_order(current_query, search_string, search_params)
 
-    if !wheres.blank?
-      where_string << " AND " unless where_string.blank?
-      where_string << "(#{wheres.join(" AND ")})"
-    end
-
-    join_string = joins.join(" ")
-    order_string = "#{orders.join(',')}" if orders.any?
-
-    Business.select("businesses.*, #{rank_string} AS search_rank")
-            .includes(:categories, :photos, :neighborhood, :top_review)
-            .joins(join_string)
-            .where(where_string, *values)
-            .order(order_string)
-            .uniq
+    current_query.uniq
   end
 
 end
