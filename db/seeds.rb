0c14f2ad3dd8db85b4f7867757579486235d9982
B. Adam Russell <b.ad.russell@gmail.com>
2014-01-16 23:11:01 -0500

    fixed userid var in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 random_reviews = File.readlines("app/assets/images/temp/random_reviews.txt").to_a
 open_times = [6.hours, 7.5.hours.to_i, 8.hours, 9.hours, 7.5.hours.to_i]
 close_times = [17.hours, 18.5.hours.to_i, 19.hours, 19.5.hours.to_i]
+user1_id = User.order("id ASC").first.id
 
 200.times do |i|
   b = Business.create!( {  name: Faker::Company.name,
@@ -38,7 +39,7 @@ close_times = [17.hours, 18.5.hours.to_i, 19.hours, 19.5.hours.to_i]
   end
 
 
-  Photo.create!(user_id: rand(user1.id..(user1.id+50)), business_id: i+3, file: File.new("app/assets/images/temp/store_#{rand(1..43)}.jpg"))
+  Photo.create!(user_id: rand(user1_id..(user1_id+50)), business_id: i+3, file: File.new("app/assets/images/temp/store_#{rand(1..43)}.jpg"))
 end
 
 
