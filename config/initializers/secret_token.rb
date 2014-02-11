ac72d6f3c5d055fb8743ca4907db267bd943fc28
B. Adam Russell <b.ad.russell@gmail.com>
2014-02-11 13:00:45 -0500

    updated security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-YelpClone::Application.config.secret_token = "a1ada5c5bee84f9593214256b8d0f3a3a91b3933a33717b2d3697e525924a2f26928bc435e32f87a864648f2f958fa35d4da9762fd583c74a8a76b283ebc7db4"
+YelpClone::Application.config.secret_token = ENV["SECRET_TOKEN"]
