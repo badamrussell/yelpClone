02ca86eaca094dc71d89ecb1a4696b777ecb1e79
B. Adam Russell <b.ad.russell@gmail.com>
2014-02-11 23:11:09 -0500

    reduced googleMap

--- lib/modules/google_map.rb
+++ lib/modules/google_map.rb
@@ -16,25 +16,6 @@ module GoogleMap
     bounds
   end
 
-  def self.get_static_map(center)
-    icon_location = "https://s3.amazonaws.com/kelp_dev/pin.png"
-    marker = []
-    Addressable::URI.new(
-      scheme: "https",
-      host: "maps.googleapis.com",
-      path: "maps/api/staticmap",
-      query_values: {
-        scale: 1,
-        center: center,
-        language: "en",
-        zoom: "13",
-        size: "200x200",
-        markers: "scale:1|shadow:true|icon:#{icon_location}|#{center}",
-        sensor: "false"
-      }
-    ).to_s
-  end
-
   def self.get_map(center, width, height)
     icon_location = "https://s3.amazonaws.com/kelp_dev/pin.png"
     str_center = "#{center['lat']}, #{center['lng']}"
